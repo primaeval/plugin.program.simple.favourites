511d30fd80903322ff8bf5069e95a12c1e2108aa
primaeval <primaeval@users.noreply.github.com>
2016-10-10 16:44:19 +0200

    help

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
+from datetime import datetime,timedelta
+from rpc import RPC
 from xbmcswift2 import Plugin
 from xbmcswift2 import actions
-import xbmc,xbmcaddon,xbmcvfs,xbmcgui
-import re
-from rpc import RPC
-import requests
-import random
-import sqlite3
-from datetime import datetime,timedelta
-import time
-#import urllib
 import HTMLParser
-import xbmcplugin
-#import xml.etree.ElementTree as ET
-#import sqlite3
 import os
-#import shutil
-#from rpc import RPC
+import random
+import re
+import requests
+import sqlite3
+import time
+import xbmc,xbmcaddon,xbmcvfs,xbmcgui
+import xbmcplugin
+
 from types import *
 
 plugin = Plugin()
@@ -34,29 +30,31 @@ def remove_formatting(label):
     label = re.sub(r"\[/?[BI]\]",'',label)
     label = re.sub(r"\[/?COLOR.*?\]",'',label)
     return label
-    
+
 @plugin.route('/play/<url>')
 def play(url):
-    xbmc.executebuiltin('PlayMedia(%s)' % url)    
- 
-@plugin.route('/add_url/<id>/<label>/<path>')
-def add_url(id,label,path):
-    folders = plugin.get_storage('urls')
+    xbmc.executebuiltin('PlayMedia(%s)' % url)
+
+@plugin.route('/add_url/<id>/<label>/<path>/<thumbnail>')
+def add_url(id,label,path,thumbnail):
     labels = plugin.get_storage('labels')
-    #ids = plugin.get_storage('ids')
+    thumbnails = plugin.get_storage('thumbnails')
+    urls = plugin.get_storage('urls')
     urls[path] = id
     labels[path] = label
-    #ids[id] = id
+    thumbnails[path] = thumbnail
     xbmc.executebuiltin('Container.Refresh')
 
-@plugin.route('/remove_url/<id>/<label>/<path>')
-def remove_url(id,label,path):
-    urls = plugin.get_storage('urls')
+@plugin.route('/remove_url/<path>')
+def remove_url(path):
     labels = plugin.get_storage('labels')
+    thumbnails = plugin.get_storage('thumbnails')
+    urls = plugin.get_storage('urls')
     del urls[path]
     del labels[path]
+    del thumbnails[path]
     xbmc.executebuiltin('Container.Refresh')
- 
+
 @plugin.route('/add_folder/<id>/<label>/<path>/<thumbnail>')
 def add_folder(id,label,path,thumbnail):
     d = xbmcgui.Dialog()
@@ -67,11 +65,9 @@ def add_folder(id,label,path,thumbnail):
     folders = plugin.get_storage('folders')
     labels = plugin.get_storage('labels')
     thumbnails = plugin.get_storage('thumbnails')
-    #ids = plugin.get_storage('ids')
     folders[path] = id
     labels[path] = label
     thumbnails[path] = thumbnail
-    #ids[id] = id
     xbmc.executebuiltin('Container.Refresh')
 
 @plugin.route('/remove_folder/<path>')
@@ -93,7 +89,6 @@ def subscribe_folder(id,label,path,thumbnail):
     dirs = dict([[remove_formatting(f["label"]), f["file"]] for f in files if f["filetype"] == "directory"])
     thumbnails = dict([f["file"], f["thumbnail"]] for f in files)
     links = {}
-    #thumbnails = {}
     for f in files:
         if f["filetype"] == "file":
             label = remove_formatting(f["label"])
@@ -101,7 +96,6 @@ def subscribe_folder(id,label,path,thumbnail):
             while (label in links):
                 label = "%s." % label
             links[label] = file
-            #thumbnails[label] = f["thumbnail"]
 
     items = []
 
@@ -121,37 +115,36 @@ def subscribe_folder(id,label,path,thumbnail):
         {
             'label': fancy_label,
             'path': plugin.url_for('subscribe_folder',id=id, label=label.encode("utf8"), path=path, thumbnail=thumbnail),
-            #'thumbnail': get_icon_path('tv'),
             'thumbnail': thumbnail,
             'context_menu': context_items,
         })
-    
+
     for label in sorted(links):
         path = links[label]
         file_thumbnail = thumbnails[path]
         if file_thumbnail:
-            thumbnail = file_thumbnail        
+            thumbnail = file_thumbnail
         context_items = []
         if path in urls:
             fancy_label = "[COLOR yellow][B]%s[/B][/COLOR] " % label
-            context_items.append(("[COLOR yellow][B]%s[/B][/COLOR] " % 'Remove', 'XBMC.RunPlugin(%s)' % (plugin.url_for(remove_url, id=id, label=label.encode("utf8"), path=path))))
+            context_items.append(("[COLOR yellow][B]%s[/B][/COLOR] " % 'Remove', 'XBMC.RunPlugin(%s)' % (plugin.url_for(remove_url, path=path))))
         else:
             fancy_label = "[B]%s[/B]" % label
-            context_items.append(("[COLOR yellow][B]%s[/B][/COLOR] " % 'Add', 'XBMC.RunPlugin(%s)' % (plugin.url_for(add_url, id=id, label=label.encode("utf8"), path=path))))
-    
+            context_items.append(("[COLOR yellow][B]%s[/B][/COLOR] " % 'Add', 'XBMC.RunPlugin(%s)' % (plugin.url_for(add_url, id=id, label=label.encode("utf8"), path=path, thumbnail=thumbnail))))
+
         items.append(
         {
             'label': label,
             'path': plugin.url_for('play',url=links[label]),
-            #'thumbnail': thumbnails[label],
             'thumbnail': thumbnail,
+            'context_menu': context_items,
         })
-    
+
     return items
 
 
-@plugin.route('/subscribe')
-def subscribe():
+@plugin.route('/add_addons')
+def add_addons():
     folders = plugin.get_storage('folders')
     ids = {}
     for folder in folders:
@@ -191,22 +184,20 @@ def subscribe():
             'label': fancy_label,
             'path': plugin.url_for('subscribe_folder',id=id, label=label, path=path, thumbnail=thumbnail),
             'thumbnail': thumbnail,
-            #'thumbnail': get_icon_path('tv'),
-            #'context_menu': context_items,
+            'context_menu': context_items,
         })
     return items
-
 
 
 @plugin.route('/')
 def index():
     items = []
-    
+
     folders = plugin.get_storage('folders')
     urls = plugin.get_storage('urls')
     labels = plugin.get_storage('labels')
     thumbnails = plugin.get_storage('thumbnails')
-    
+
     for folder in sorted(folders, key=lambda x: labels[x]):
         path = folder
         label = labels[folder]
@@ -217,25 +208,29 @@ def index():
         {
             'label': label,
             'path': folder,
-            #'thumbnail':get_icon_path('tv'),
             'thumbnail':thumbnail,
             'context_menu': context_items,
         })
     for url in urls:
         label = labels[url]
+        thumbnail = thumbnails[url]
+        context_items = []
+        context_items.append(("[COLOR yellow][B]%s[/B][/COLOR] " % 'Remove', 'XBMC.RunPlugin(%s)' % (plugin.url_for(remove_url, path=path))))
         items.append(
         {
             'label': label,
             'path': url,
-            'thumbnail':get_icon_path('tv'),
+            'thumbnail':thumbnail,
+            'context_menu': context_items,
         })
 
     items.append(
     {
         'label': "Add",
-        'path': plugin.url_for('subscribe'),
+        'path': plugin.url_for('add_addons'),
         'thumbnail':get_icon_path('settings'),
     })
+
     return items
 
 if __name__ == '__main__':
